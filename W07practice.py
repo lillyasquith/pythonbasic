7b1f241c52eacee00d026eabd15a830c96f3a151
lillyasquith <asq22001@byui.edu>
2026-02-05 00:33:17 -0700

    Finished W07 Team Activity

--- W07practice.py
+++ W07practice.py
@@ -1,4 +1,8 @@
+#**************************************#
 # W07 CheckPoint - While loops 
+#**************************************#
+
+
 
 #1.Use a while loop to ask the user for a positive number (>= 0). Continue asking as long as the number is negative, then display the number
 
@@ -16,14 +20,49 @@
 #-------------------------------------#
 #2.Use a while loop, to simulate a child asking their parent for a piece of candy. Have the program keep looping until the user answers "yes", then have the program output "Thank you."
 
-candy_permission = "no"
+# candy_permission = "no"
 
-while candy_permission == "no":
+# while candy_permission == "no":
 
-    candy_permission = input("May I have a piece of candy? ").lower()
+#     candy_permission = input("May I have a piece of candy? ").lower()
     
-print ("Thank you.")
+# print ("Thank you.")
 
+
+
+
+#******************************************#
+#W07: Team Activity - Guess My Number Game
+#*****************************************#
+
+import random
+
+magic_number = random.randint(1,10)
+
+# magic_number = int(input("What is the magic number? "))
+
+guess_number = 0
+
+#Stretch Challenge 1: Keep track of how many guesses the user has made and inform them of it at the end of the game.
+
+count = 0
+
+#Stretch Challenge 2: After the game is over, ask the user if they want to play again. Then, loop back and play the whole game again and continue this loop as long as they keep saying "yes".
+
+play_again = ""
+
+while guess_number != magic_number or play_again == "yes":
+    guess_number = int(input("What is your guess? "))
+    count += 1
+    if guess_number > magic_number:
+        print("Lower")
+    elif guess_number < magic_number:
+        print("Higher")
+    else:
+        print("You guessed it!")
+        print(f"Total guesses: {count}.")
+        play_again = input("Would you like to play again (yes/no)? ")
+       
 
 
 
