7a37605286ca853a00eb619336ed86e9cc178163
lillyasquith <asq22001@byui.edu>
2026-04-09 23:11:21 -0600

    W10: Team Activity - Core Requirements

--- W10TeamChallenge.py
+++ W10TeamChallenge.py
@@ -1,3 +1,35 @@
 #***********************************#
 #W10: Team Activity - Multiple Lists
 #***********************************#
+
+account_names = []
+
+balances = []
+
+print("Enter the names and balances of bank accounts (type: quit when done)")
+
+user_account = ""
+
+while user_account.lower() != "quit":
+    user_account = input("What is the name of this account? ")
+    if user_account != "quit":
+        account_names.append(user_account)
+        input_balance = float(input("What is the balance? "))
+        balances.append(input_balance)
+
+print()
+print("Account Information:")
+for i in range(len(account_names)):
+    account_name = account_names[i]
+    balance = balances[i]
+    print(f"{account_name} - ${balance}")
+
+print()
+total = sum(balances)
+print(f"Total: ${total}")
+average = (total / (len(balances)))
+print(f"Average: ${average:.2f}")
+
+
+
+
