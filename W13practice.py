58f911fc5877a334adb66360c96637f51b6f29d8
lillyasquith <asq22001@byui.edu>
2026-09-10 21:42:16 -0600

    finished checkpoint challenge

---
+++ W13practice.py
@@ -0,0 +1,15 @@
+
+def display_regular(text):
+    print(text)
+
+def display_uppercase(text):
+    print(text.upper())
+    
+def display_lowercase(text):
+    print(text.lower())
+
+message = input("What is your message? ")
+
+display_regular(message)
+display_uppercase(message)
+display_lowercase(message)
